d1327c91cc3bd8ad817c58dcf2a1ac6d762cf646
tatayoyo77 <peggy.manette@pole-emploi.fr>
2017-09-27 15:30:05 +0200

    TP JPA Projet Patrimoine : réalisation des étapes 1 a 5

--- src/main/java/co/simplon/poleEmploi/patrimoine/modele/Ville.java
+++ src/main/java/co/simplon/poleEmploi/patrimoine/modele/Ville.java
@@ -1,9 +1,25 @@
 package co.simplon.poleEmploi.patrimoine.modele;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table (name = "CITIES")
 public class Ville {
 
+	@Id
+	@GeneratedValue ( strategy = GenerationType.IDENTITY )
 	private Long id;
+	@Column (name = "NAME")
 	private String nom;
+	@Column (name = "LONGITUDE")
+	private double longitude;
+	@Column (name = "LATITUDE")
+	private double latitude; 
 
 	public Long getId() {
 		return id;
@@ -19,5 +35,21 @@ public class Ville {
 
 	public void setNom(String nom) {
 		this.nom = nom;
+	}
+
+	public double getLongitude() {
+		return longitude;
+	}
+
+	public void setLongitude(double longitude) {
+		this.longitude = longitude;
+	}
+
+	public double getLatitude() {
+		return latitude;
+	}
+
+	public void setLatitude(double latitude) {
+		this.latitude = latitude;
 	}
 }
--- src/test/java/co/simplon/poleEmploi/patrimoine/VilleJpaTest.java
+++ src/test/java/co/simplon/poleEmploi/patrimoine/VilleJpaTest.java
@@ -1,30 +1,63 @@
 package co.simplon.poleEmploi.patrimoine;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import co.simplon.poleEmploi.patrimoine.modele.Ville;
 
 public class VilleJpaTest {
+	private Long idRecherche;
+	private EntityManagerFactory emf;
+	private EntityManager em;
+	
+	@Before
+	public void setUp() throws Exception {
+		System.out.println("------------------");
+		emf = Persistence.createEntityManagerFactory("BasePatrimoine");
+		em = emf.createEntityManager();
+	}
 
 	@Test
 	public void la_ville_d_identifiant_25925_doit_etre_Forstfeld() {
 		// GIVEN
-		Long idRecherche = 25925L;
+		idRecherche = 25925L;
 
-		// WHEN
-		EntityManagerFactory emf = Persistence
-				.createEntityManagerFactory("BasePatrimoine");
-		EntityManager em = emf.createEntityManager();
+		// WHEN		
 		Ville ville = em.find(Ville.class, idRecherche);
 
 		// THEN
 		assertEquals(idRecherche, ville.getId());
 		assertEquals("Forstfeld", ville.getNom());
+	}
+	
+	
+	@Test
+	public void la_ville_33_est_de_longitude_526667() {
+		// GIVEN
+		idRecherche = 33L;
+		Ville villeATrouver = new Ville();
+		villeATrouver.setId(idRecherche);
+		villeATrouver.setNom("La Tranclière");
+		villeATrouver.setLongitude(5.266667);
+		villeATrouver.setLatitude(46.116667);
+
+		// WHEN		
+		Ville ville = em.find(Ville.class, villeATrouver.getId());
+
+		// THEN
+		assertEquals(villeATrouver.getId(), ville.getId());
+		assertEquals(villeATrouver.getNom(), ville.getNom());		
+		assertEquals(villeATrouver.getLongitude(), ville.getLongitude(), 0.0);		
+		assertEquals(villeATrouver.getLatitude(), ville.getLatitude(), 0.0);
 	}
 }
